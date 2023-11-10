337505d8e84f3d7dd6480252a08951b3d8f1a9c8
TJ-Tracy-02 <142838301+TJ-Tracy-02@users.noreply.github.com>
2023-11-10 09:08:36 -0500

    Update World.java

--- csc121-creative-project/src/World.java
+++ csc121-creative-project/src/World.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 import processing.core.*;
@@ -36,87 +37,87 @@ public class World {
 		switch(score) {
 		case 0:
 			c.background(102, 102, 255);
-			
-			
+
+
 			// first cloud
 			c.fill(255);
 			c.rect(100 - move, 50,70,20);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(100 - move,50,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(110 - move,53,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(120 - move,30,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(130 - move,50,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(140 - move,30,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(145 - move,57,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(155 - move,45,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(160 - move,55,40);
-			
+
 			// second cloud
 			c.fill(255);
 			c.rect(100 + move*2, 50 + move,70,20);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(100 + move*2,50 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(110 + move*2,53 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(120 + move*2,30 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(130 + move*2,50 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(140 + move*2,30 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(145 + move*2,57 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(155 + move*2,45 + move,40);
-			
+
 			c.fill(255);
 			c.stroke(255);
 			c.circle(160 + move*2 ,55 + move,40);
-			
-			
-			
+
+
+
 			c.noStroke();
-			
-			
-			
-			
+
+
+
+
 			c.fill(0);
 			c.text(score,10,10);
 			c.text(time/60, 280,10);
@@ -131,6 +132,22 @@ public class World {
 			c.background(0);
 			c.fill(224,224,224);
 			c.circle(100, 100, 50);
+
+			c.fill(47,79,79);
+			c.stroke(169);
+			c.circle(100, 115, 10);
+			c.noStroke();
+
+			c.fill(47,79,79);
+			c.stroke(169);
+			c.circle(90, 95, 15);
+			c.noStroke();
+
+			c.fill(47,79,79);
+			c.stroke(169);
+			c.circle(115, 100, 10);
+			c.noStroke();
+
 			c.fill(255, 255, 255);
 			c.text(score,10,10);
 			c.text(time/60, 280,10);
@@ -142,12 +159,41 @@ public class World {
 			break;
 		case 2:
 			c.background(0);
-			c.fill(196,164,132);
+			c.fill(170, 74, 68);
 			c.circle(100, 100, 100);
+
+			c.fill(255);
+			c.stroke(47,79,79);
+			c.ellipse(100, 55, 35,10);
+			c.noStroke();
+
+			//136, 8, 8
+
+			c.fill(255);
+			c.stroke(47,79,79);
+			c.ellipse(100, 145, 30,10);
+			c.noStroke();
+
 			c.fill(255);
 			c.text(score,10,10);
 			c.text(time/60, 280,10);
 			c.text(shotsMade, 150,10);
+
+			c.fill(136, 8, 8);
+			c.stroke(136,0,0);
+			c.circle(120, 100, 12);
+			c.noStroke();
+
+			c.fill(136, 8, 8);
+			c.stroke(136,0,0);
+			c.circle(80, 80, 25);
+			c.noStroke();
+
+			c.fill(136, 8, 8);
+			c.stroke(136,0,0);
+			c.circle(95, 120, 18);
+			c.noStroke();
+
 
 			this.ball.draw(c);
 			this.p.draw(c);
@@ -157,6 +203,36 @@ public class World {
 			c.background(0);
 			c.fill(152, 251, 152);
 			c.circle(100, 100, 150);
+
+
+			
+			c.fill(255);
+			c.circle(215,150,4);
+			c.fill(255);
+			c.circle(240,70,4);
+			c.fill(255);
+			c.circle(150,30,4);
+			c.fill(255);
+			c.circle(200,95,4);
+			c.fill(255);
+			c.circle(260,30,4);
+			c.fill(255);
+			c.circle(20,180,4);
+			c.fill(255);
+			c.circle(120,200,4);
+			c.fill(255);
+			c.circle(60,20,4);
+			c.fill(255);
+			c.circle(20,20,4);
+			c.fill(255);
+			c.circle(85,230,4);
+			c.fill(255);
+			c.circle(170,225,4);
+			c.fill(255);
+			c.circle(185,190,4);
+
+
+
 			c.fill(255);
 			c.text(score,10,10);
 			c.text(time/60, 280,10);
@@ -166,6 +242,7 @@ public class World {
 			this.p.draw(c);
 			this.hoop.draw(c);
 			break;
+
 		case 4:
 			c.background(0);
 			c.fill(0,0,255);
@@ -183,9 +260,11 @@ public class World {
 			c.background(0);
 			c.fill(204,153,102);
 			c.circle(100, 100, 150);
+
 			c.noFill();
 			c.stroke(150,75,0);
 			c.ellipse(100,100,200,5);
+
 			c.fill(255);
 			c.text(score,10,10);
 			c.text(time/60, 280,10);
